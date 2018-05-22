a6815f90c33eae080ef391149f178f68b6b92338
ANUBIS\Jehuty <juan.irizarry.cole@gmail.com>
2018-05-22 19:24:40 -0400

    Still sorting out mono

--- Assets/UI/UIManager.cs
+++ Assets/UI/UIManager.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class UIManager : MonoBehaviour {
 
 	public static UIManager main;
 	public GameObject playerBar;
-   
-    // Use this for initialization
+	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
 	}
 
-    public void SetPlayerBar() {
-        
-    }
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void SetPlayerBar() {
+
+	}
 }
